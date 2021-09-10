e6712a2b31ca32ce8b5fdb79c872e5295d3c410f
Shaurya <agarwal.lucky.shaurya11@gmail.com>
2021-09-11 01:36:49 +0530

    feat: add sending cowbot messages

---
+++ bot.js
@@ -0,0 +1,38 @@
+const { Client, Intents } = require('discord.js')
+const winston = require('winston')
+const { getImage } = require('random-reddit')
+
+const token = require('./token.json')
+
+// Configure logger settings
+const logger = winston.createLogger({
+    format: winston.format.simple(),
+    transports: [new winston.transports.Console()],
+})
+
+// Initialize Discord Bot
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+
+client.once('ready', function (evt) {
+    logger.info('Connected')
+    logger.info(client.id)
+})
+
+client.on('message', async function (message) {
+    console.log(message.content)
+
+    try {
+        if (message.content === `moo`) {
+            imageUrl = await getImage('cow')
+            message.channel.send(imageUrl)
+        }
+        else if (message.content === `chonk`) {
+            imageUrl = await getImage('cat')
+            message.channel.send(imageUrl)
+        }
+    } catch (err) {
+        logger.error(err)
+    }
+})
+
+client.login(token.token)
